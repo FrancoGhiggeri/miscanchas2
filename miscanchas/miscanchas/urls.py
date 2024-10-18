da0d9304657a3a1bfe088bf4480fe3e31ad2729c
Matías <matiaspendino76@gmail.com>
2024-10-18 21:58:25 +1100

    CSRF_TRUSTED_ORIGINS urls update. Password reset path replace re_path -> path

--- miscanchas/miscanchas/urls.py
+++ miscanchas/miscanchas/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
-    re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('owner/new/', new_owner, name='new_owner'),
